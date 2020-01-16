6bc73aaf36f6c24ae443cdf114a6f887dc393038
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-16 17:26:58 -0500

    adds practice file

---
+++ app.py
@@ -0,0 +1,12 @@
+# print('Justin Crabtree')
+
+# name = input('What is your name? ')
+# print('Hi ' + name)
+
+# name = input('What is your name? ')
+# fav_color = input('What is your favorite color? ')
+# print(name + ' likes the color ' + fav_color)
+
+birth_year = input('What year were you born? ')
+age = 2019 - int(birth_year)
+print(age)
