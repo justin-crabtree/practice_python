0eed6d7821b1cc0bf61df38cb11e890a985f176d
crabtree <justin.kyle.crabtree@gmail.com>
2020-01-22 18:52:35 -0500

    adds more practice

--- app.py
+++ app.py
@@ -7,6 +7,31 @@
 # fav_color = input('What is your favorite color? ')
 # print(name + ' likes the color ' + fav_color)
 
-birth_year = input('What year were you born? ')
-age = 2019 - int(birth_year)
-print(age)
+# birth_year = input('What year were you born? ')
+# age = 2019 - int(birth_year)
+# print(age)
+
+# weight_pounds = input('How much do you weigh in pounds? ')
+# weight_pounds = int(weight_pounds)
+# weight_kilograms = weight_pounds / 2.2046
+# weight_kilograms = int(weight_kilograms)
+# print(weight_kilograms)
+
+# name = 'Jennifer'
+# print(name[1:-1])
+
+# first = 'John'
+# last = 'Smith'
+# message = first + ' [' + last + '] is a coder'
+# msg = f'{first} [{last}] is a coder'
+# print(message)
+# print(msg)
+
+course = 'Python for Beginners'
+print(course.upper())
+print(course.lower())
+print(course)
+print(course.find('P'))
+# print(course.replace('P', 'J'))
+print(len(course))
+print('Python' in course)
